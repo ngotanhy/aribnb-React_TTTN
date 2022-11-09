2d88c8ab82d93b79db127850f3b863439a148910
Hy Ngo <ngotanhy123456@gamil.com>
2022-11-09 13:02:42 +0700

    update

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-
+import img from "../../assets/img/loading/robot.gif"
 type Props = {};
 
 export default function Loading({}: Props) {
   return (
-    <div className="w-full h-100vh flex items-center justify-center ">
-      <div className="flex items-center justify-center gap-4 animate__animated animate__bounce animate__infinite	infinite">
-        <div className="w-10 h-10 rounded-full bg-black animate__animated animate__bounce"></div>
-        <div className="w-10 h-10 rounded-full bg-black animate__animated animate__bounce animate__delay-1s animate__infinite	infinite  "></div>
-        <div className="w-10 h-10 rounded-full bg-black animate__animated animate__bounce animate__delay-2s "></div>
-        <div className="w-10 h-10 rounded-full bg-black animate__animated animate__bounce animate__delay-3s animate__infinite	infinite"></div>
-      </div>
+    <div className="w-full h-100vh flex items-center justify-center bg-slate-300">
+      <img src={img} alt="Loading..." className="object-cover w-96 h-96 x"/>
     </div>
   );
 }
